b22c05014d488be5061d4127887deafe471071df
eureyuri <eureynoguchi@gmail.com>
2020-09-24 14:37:25 -0400

    Complete backend of tic tac toe Fix styles with checkstyle guideline

--- hw1/src/main/java/controllers/PlayGame.java
+++ hw1/src/main/java/controllers/PlayGame.java
@@ -3,13 +3,15 @@ package controllers;
 import io.javalin.Javalin;
 import java.io.IOException;
 import java.util.Queue;
+import models.GameBoard;
+import models.Message;
 import org.eclipse.jetty.websocket.api.Session;
 
 class PlayGame {
 
   private static final int PORT_NUMBER = 8080;
-
   private static Javalin app;
+  private static GameBoard gameBoard;
 
   /** Main method of the application.
    * @param args Command line arguments
@@ -24,26 +26,49 @@ class PlayGame {
     app.post("/echo", ctx -> {
       ctx.result(ctx.body());
     });
-
-    /**
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     */
+    
+    // Redirect player to the View component
+    app.get("/newgame", ctx -> {
+      ctx.redirect("tictactoe.html");
+    });
+    
+    // Start the game by initializing a game board with player specified type (X or O) 
+    // from the request body
+    app.post("/startgame", ctx -> {
+      char type = ctx.formParam("type").charAt(0);
+      gameBoard = new GameBoard(type);
+      ctx.result(gameBoard.toJson());
+    });
+    
+    // End point for player 2 to join. Redirects to the View after joining the game. If player 2 
+    // tries to join the game before player 1 creates the game, simply return.
+    app.get("/joingame", ctx -> {
+      if (gameBoard == null) {
+        return;
+      }
+      
+      gameBoard.joinGame();
+      sendGameBoardToAllPlayers(gameBoard.toJson());  
+      ctx.redirect("/tictactoe.html?p=2");
+    });
+    
+    // End point to handle player moves. 
+    // Returns the validity response for the move as well as updates the board
+    app.post("/move/:playerId", ctx -> {
+      // If game board has not been created yet or the game has not been started yet, then return
+      if (gameBoard == null || !gameBoard.isGameStarted()) {
+        return;
+      }
+      
+      // Get playerId and x, y coordinates from the path parameter and request body
+      int playerId = Integer.parseInt(ctx.pathParam("playerId"));
+      int x = Integer.parseInt(ctx.formParam("x"));
+      int y = Integer.parseInt(ctx.formParam("y"));
+      
+      Message validityResponse = gameBoard.move(playerId, x, y);
+      ctx.result(validityResponse.toJson());
+      sendGameBoardToAllPlayers(gameBoard.toJson());  
+    });
 
     // Web sockets - DO NOT DELETE or CHANGE
     app.ws("/gameboard", new UiWebSocket());
